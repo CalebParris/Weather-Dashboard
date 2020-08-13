9bce072562a161d1b3467f57725b52db24d7e89d
Caleb Parris <calebparris@live.com>
2020-08-13 15:06:42 -0500

    Adds 5-day forecast for searched city

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,7 +20,7 @@ $("#search-btn").on("click", function(event){
 function currentWeather(){
     var inputCity = $("#input-city").val();
     var apiKey = "acb674f7dc9a574ba0b5c0ef2926ec4f"
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + apiKey + "&units=Imperial";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + apiKey + "&units=imperial";
 
     $.ajax({
         url: queryURL,
@@ -45,11 +45,11 @@ function currentWeather(){
         cityDate.append(weatherIcon);
         $("#current-weather").append(cityDate);
 
-        var temp = $("<p>");
-        temp.text("Temperature: " + response.main.temp + " °F");
+        var currentTemp = $("<p>");
+        currentTemp.text("Temperature: " + Math.floor(response.main.temp) + " °F");
 
-        var humidity = $("<p>");
-        humidity.text("Humidity: " + response.main.humidity + "%");
+        var currentHumidity = $("<p>");
+        currentHumidity.text("Humidity: " + response.main.humidity + "%");
         
         var windSpeed = $("<p>");
         windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
@@ -57,20 +57,60 @@ function currentWeather(){
         var uvIndex = $("<p>");
         uvIndex.text("UV Index: ");
 
-        $("#current-weather").append(temp, humidity, windSpeed);
+        $("#current-weather").append(currentTemp, currentHumidity, windSpeed);
 
     });
 }
 
 function fiveDayWeather(){
+    $("#five-day").empty();
     var inputCity = $("#input-city").val();
     var apiKey = "acb674f7dc9a574ba0b5c0ef2926ec4f"
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + apiKey + "&units=Imperial";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + apiKey + "&units=imperial";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response);
+
+        var forcastTitle = $("<h3>");
+        var forecastDiv = $("<div>");
+        forecastDiv.addClass("forecast")
+        forcastTitle.text("5-Day Forecast:");
+        $("#five-day").append(forcastTitle, forecastDiv);
+
+        for (var i = 0; i < response.list.length; i++){
+
+            if (response.list[i].dt_txt.substring(11) === "12:00:00"){
+                console.log("Mid-day");
+                var unixTime = response.list[i].dt;
+                var date = new Date(unixTime * 1000);
+                var day = date.getDate();
+                var month = date.getMonth();
+                var year = date.getFullYear();
+                var today = (month + 1) + "/" + day + "/" + year;
+                console.log(today);
+
+                var dayDiv = $("<div>");
+                var forecastDate = $("<p>");
+                var forecastIcon = $("<img>");
+                var forecastTemp = $("<p>");
+                var forecastHumidity = $("<p>");
+                var iconURL = "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png";
+
+                dayDiv.addClass("forecast-day")
+                forecastDate.text(today);
+                forecastIcon.attr("src", iconURL);
+                forecastTemp.text("Temp: " + Math.floor(response.list[i].main.temp) + " °F");
+                forecastHumidity.text("Humidity: " + response.list[i].main.humidity + "%");
+
+                dayDiv.append(forecastDate, forecastIcon, forecastTemp, forecastHumidity);
+                forecastDiv.append(dayDiv);
+
+
+            }
+
+        }
     });
 }
